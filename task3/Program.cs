a848584704e9bebef15e580e27f8306430c0f063
Vamik76 <merts_v@mail.ru>
2022-12-13 19:50:21 +1000

    HW7C#_task3

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте двумерный массив (вручную) из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.WriteLine("Ведите числа m и n");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] array2d = new int[m, n];
+decimal [] averegest = new decimal [n]; 
+Random random = new Random();
+for (int i=0; i<m;i++)
+{
+    for (int j=0; j<n; j++)
+    {
+        array2d[i,j]=random.Next(0,10);
+        Console.Write(array2d[i,j]+" ");
+        averegest[j]=averegest[j]+(Convert.ToDecimal(array2d[i,j])/Convert.ToDecimal(m));
+    }
+Console.WriteLine();
+}
+Console.WriteLine("Средние арифмитические каждого столбца: ");
+Console.WriteLine(string.Join("; ",averegest));
